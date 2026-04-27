3474c2fe20875b256cc0b4e35ce7692d3290de9e
sreekar0613 <skakumani06@gmail.com>
2026-04-27 13:20:12 -0400

    feat: wire AuthGuard to root route

--- bill/src/router/routes/__root.tsx
+++ bill/src/router/routes/__root.tsx
@@ -1,5 +1,10 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { AuthGuard } from "../AuthGuard";
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: () => (
+    <AuthGuard>
+      <Outlet />
+    </AuthGuard>
+  ),
 });
